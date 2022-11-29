31e7dfcc2bb00738032cce34b88aa4096a2510ad
VINICIOS MATHEUS OLIVEIRA DA SILVA <vinicios.tec22@gmail.com>
2022-11-29 16:27:32 -0300

    pacienteupdate

--- paciente/models.py
+++ paciente/models.py
@@ -13,8 +13,9 @@ class Paciente(models.Model):
     nome = models.CharField(max_length=60)
     data_nascimento = models.DateField()
     cpf = models.CharField(max_length=12)
-    email = models.EmailField
+    email = models.EmailField(default='')
     telefone = models.CharField(max_length=12)
+    endereco = models.CharField(max_length=200, default='')
     convenio = models.ForeignKey(Convenio,null=True, on_delete=models.SET_NULL, default='')
     
     def __str__(self) -> str:
--- paciente/views.py
+++ paciente/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .forms import PacienteModelForm
+from . models import Paciente, Convenio
 
 def cadastrar_paciente(request):
     if request.method == 'GET':
         forms = PacienteModelForm()
         return render(request, 'paciente/cadastro_paciente.html', {'forms':forms})
+    elif request.method == 'POST':
+        nome            = request.POST.get('nome')
+        email           = request.POST.get('email')
+        data_nascimento = request.POST.get('data_nascimento')
+        cpf             = request.POST.get('cpf')
+        telefone        = request.POST.get('telefone')
+        convenio        = request.POST.get('convenio')
+        endereco        = request.POST.get('endereco')
+        
+        
+        
+        paciente = Paciente(
+            nome = nome,
+            data_nascimento = data_nascimento,
+            cpf = cpf,
+            email = email,
+            telefone = telefone,
+            endereco = endereco,
+            convenio_id = convenio
+            
+            
+        )
+        paciente.save()
+        return redirect('cadastrar_paciente')
