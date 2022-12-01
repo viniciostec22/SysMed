9a3616653396245ea6269a4b80e5f7369024d4ae
VINICIOS MATHEUS OLIVEIRA DA SILVA <vinicios.tec22@gmail.com>
2022-12-01 15:29:58 -0300

    consulta

--- consulta/views.py
+++ consulta/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from paciente.models import Paciente
+from django.db.models import Q
 
 # Create your views here.
 def cadastrar_consulta(request):
-    return render(request, 'consulta/cadastrar_consulta.html')
+    
+        paciente = Paciente.objects.all()
+        return render(request, 'consulta/cadastrar_consulta.html',{'paciente':paciente})
+    
+   
