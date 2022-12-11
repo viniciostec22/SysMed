29b2316e38a04b07f0597245c80f7d8f6d254b13
Vinicios Matheus <vinicios.tec22@gmail.com>
2022-12-11 19:25:40 -0300

    agendamento/horarios_marcados

--- agenda/views.py
+++ agenda/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from consulta.models import MarcarConsulta
+from medico.models import Medico
 
 # Create your views here.
 def agenda(request):
-    return HttpResponse('agenda')
+    if request.method == 'GET':
+        medico = Medico.objects.all()
+        horario = MarcarConsulta.objects.all().order_by('-medico','horario')
+        return render(request, 'agendamentos.html', {'medico':medico, 'horario':horario})
