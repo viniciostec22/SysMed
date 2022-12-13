5d4b9af4c8b88f98898573f4c9df08819334077f
Vinicios Matheus <vinicios.tec22@gmail.com>
2022-12-13 05:27:37 -0300

    v2.1

--- users/views.py
+++ users/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.messages import constants
 from django.contrib import messages
 from .models import Cargo
+from django.core.validators import validate_email
 
 @has_permission_decorator('cadastrar_recepcionista')
 def cadastro_usuario(request):
@@ -28,18 +29,27 @@ def cadastro_usuario(request):
         cargo = request.POST.get('cargo')
         
         user = Users.objects.filter(username=usuario)
-        
-        if user.exists():
-            messages.add_message(request, constants.ERROR, 'Usuario já existente!!') 
+        if not usuario or not email or not nome or not sobrenome or not data_nascimento or not cpf or not senha or not cargo:
+            messages.add_message(request, constants.ERROR, 'Não pode deixar campos em Branco!!') 
+            
+        elif user.exists():
+            messages.add_message(request, constants.ERROR, 'Nome de Usuario em uso tento outro!!') 
             return redirect('cadastrar_usuario')
         
-        if Users.objects.filter(cpf=cpf):
+        elif Users.objects.filter(cpf=cpf):
             messages.add_message(request, constants.ERROR, 'CPF já cadastrado') 
+            return redirect('cadastrar_usuario')
+        
+        try:
+            validate_email(email)
+        except:
+            messages.add_message(request, constants.ERROR, 'Email invalido')
             return redirect('cadastrar_usuario')
         
         if Users.objects.filter(email=email):
             messages.add_message(request, constants.ERROR, 'Email já está em uso') 
             return redirect('cadastrar_usuario')
+        
         
         user = Users.objects.create_user(username=usuario, email=email, first_name=nome, last_name=sobrenome, password=senha, cargo_id=cargo, data_nascimento=data_nascimento, cpf=cpf)  # type: ignore
         messages.add_message(request, constants.SUCCESS, 'Usuario cadastrado com sucesso!') 
@@ -48,12 +58,7 @@ def cadastro_usuario(request):
 
 def login(request):
     
-    if request.method == "GET":
-        if request.user.is_authenticated:
-            return redirect(reverse('plataforma'))
-        
-        return render(request, 'login.html')
-    elif request.method == "POST":
+    if request.method == "POST":
         login = request.POST.get('email')
         senha = request.POST.get('senha')
         
@@ -66,11 +71,10 @@ def login(request):
         auth.login(request, user)
         messages.add_message(request, constants.SUCCESS, 'Login efetuado com sucesso')
         return render(request,'plataforma.html',{'user':user})
+    return redirect(reverse('plataforma'))
 @login_required
 def plataforma(request):
-    
     return render(request,'plataforma.html')  
-  
 
 def logout(request):
     request.session.flush()
