2a939731788e089d4a1eb48efa3988cdb679432d
VINICIOS MATHEUS OLIVEIRA DA SILVA <vinicios.tec22@gmail.com>
2022-12-14 16:12:17 -0300

    erro-cadastrar-usuario

--- users/signals.py
+++ users/signals.py
@@ -7,9 +7,9 @@ from rolepermissions.roles import assign_role
 @receiver(post_save, sender=Users)
 def define_permissoes(sender, instance, created, **kwargs):
     if created:
-        if instance.cargo__nome == "Recepcionista":
+        if instance.cargo == "Recepcionista":
             assign_role(instance, 'Recepcionista')
-        elif instance.cargo__nome == "Gerente":
+        elif instance.cargo == "Gerente":
             assign_role(instance, 'gerenete')
-        elif instance.cargo__nome == "medico":
+        elif instance.cargo == "medico":
             assign_role(instance, 'medico')
