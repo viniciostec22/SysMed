ea5a3a99b23c2b8e69a8c6ffb6512cd29ed6f8f9
Vinicios Matheus <vinicios.tec22@gmail.com>
2022-12-15 23:39:56 -0300

    v2.4

--- consulta/urls.py
+++ consulta/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
    path('cadastrar_consulta/', views.cadastrar_consulta, name='cadastrar_consulta'),
-   # path('listar_consultas/', views.listar_consultas, name='listar_consultas'),
-   # path('editar_consulta/<int:id>', views.editar_consulta, name='editar_consulta'),
-   # path('excluir_consulta/<int:id>', views.excluir_consulta, name='excluir_consulta')
+   path('listar_consultas/', views.listar_consultas, name='listar_consultas'),
+   path('editar_consulta/<int:id>', views.editar_consulta, name='editar_consulta'),
+   path('excluir_consulta/<int:id>', views.excluir_consulta, name='excluir_consulta')
   
 ]
--- consulta/views.py
+++ consulta/views.py
@@ -7,10 +7,11 @@ from .models import MarcarConsulta
 from .forms import MarcaConsultaModelForm
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.urls import reverse
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 # Create your views here.
-
+@login_required
 def cadastrar_consulta(request):
         pacientes = Paciente.objects.all()
         medicos = Medico.objects.all()
@@ -19,21 +20,30 @@ def cadastrar_consulta(request):
                
                 return render(request, 'consulta/cadastrar_consulta.html', {'pacientes':pacientes, 'medicos':medicos, 'consulta':consulta})
         elif request.method == 'POST':
+                
                 paciente = request.POST.get('paciente')
+                if paciente == '':
+                        messages.add_message(request, constants.ERROR, 'Campo Nome do paciente é obrigatorio!!')
+                        return redirect(reverse('cadastrar_consulta'))
                 paciente = Paciente.objects.get(nome=paciente)
                 medico = request.POST.get('medico')
+                if medico== '':
+                        messages.add_message(request, constants.ERROR, 'Campo Medico Obrigatorio!!')
+                        return redirect(reverse('cadastrar_consulta'))
                 medico = Medico.objects.get(nome = medico)
-                print(paciente)
-                tipo_consulta = request.POST.get('tipo_consulta')
+        
+                
                 valor = request.POST.get('valor')
                 horario = request.POST.get('hora')
                 descricao = request.POST.get('descricao')
                 
-                if not paciente or not medico or not valor or horario or not descricao: 
-                        print('aqui')
+                if not  valor or not horario or not descricao: 
                         messages.add_message(request, constants.ERROR, 'Não pode deixar campos em Branco!!')
-                        return redirect('cadastrar_consulta')
+                        return redirect(reverse('cadastrar_consulta'))
                 
+                if MarcarConsulta.objects.filter(medico=medico, horario=horario):
+                        messages.add_message(request, constants.ERROR, 'Horario não esta disponivel para o medico!!')
+                        return redirect(reverse('cadastrar_consulta'))
                 nova_consulta = MarcarConsulta(
                         paciente_id = paciente.id,
                         medico_id = medico.id,
@@ -45,51 +55,30 @@ def cadastrar_consulta(request):
                 nova_consulta.save()
                 messages.add_message(request, constants.SUCCESS, 'Consulta Marcada com Sucesso!!')
                 return render(request, 'consulta/cadastrar_consulta.html', {'pacientes':pacientes, 'medicos':medicos})
-# def cadastrar_consulta(request):
-#         if request.method == 'GET':
-#                 forms = MarcaConsultaModelForm()  
-#                 return render(request, 'consulta/cadastrar_consulta.html',
-#                               {'forms':forms})
+@login_required
+def editar_consulta(request, id):
+        pacientes = Paciente.objects.filter(id=id)
+        consulta = get_object_or_404(MarcarConsulta, id=id)
+        return render(request, 'consulta/editar_consulta.html', {'pacientes':pacientes,'consulta':consulta})
                 
-#         elif request.method == 'POST':
-#                 forms = MarcaConsultaModelForm(request.POST) 
-               
-#                 if forms.is_valid():
-#                         #if Medico.objects.filter(medico.horario)
-#                         forms.save() 
-#                         messages.add_message(request, constants.SUCCESS, 'Consulta Marcada com Sucesso!!')
-#                         return redirect('cadastrar_consulta')
-#                 else:
-#                         return render(request, 'consulta/cadastrar_consulta.html',{'forms':forms})
-# @login_required
-# def editar_consulta(request, id):
-#         consuta = MarcarConsulta.objects.get(id=id)
-#         if request.method == 'POST':
-#                 forms = MarcaConsultaModelForm(request.POST or None, instance=consuta)
-#                 if forms.is_valid():
-#                         forms.save()
-#                         messages.add_message(request, constants.SUCCESS, 'Consulta editada Sucesso!!')
-#                         return redirect('cadastrar_consulta')
-#         else:
-#                 forms = MarcaConsultaModelForm(instance=consuta)
-#         return render(request, 'consulta/cadastrar_consulta.html',{'forms':forms, 'consulta':consulta})
-# @login_required
-# def listar_consultas(request):
 
-#       if request.GET.get('termo'):
-#               termo = request.GET.get('termo')
-#               consultas = MarcarConsulta.objects.filter(Q(paciente__nome__icontains=termo) | 
-#                                                         Q(medico__nome__icontains=termo)|
-#                                                         Q(data__icontains=termo))
-#       else:
-#               consultas = MarcarConsulta.objects.order_by('-id', '-paciente')
-#       paginator = Paginator(consultas, 10)  
-#       page = request.GET.get('page')
-#       consultas = paginator.get_page(page)
-#       return render(request, 'consulta/consultas.html', {'consultas':consultas})
-# @login_required
-# def excluir_consulta(request, id):
-#         consulta = get_object_or_404(MarcarConsulta, id=id)
-#         consulta.delete()
-#         messages.add_message(request, constants.SUCCESS, 'Consulta Excluida Sucesso!!')
-#         return redirect('listar_consultas')
+@login_required
+def listar_consultas(request):
+
+      if request.GET.get('termo'):
+              termo = request.GET.get('termo')
+              consultas = MarcarConsulta.objects.filter(Q(paciente__nome__icontains=termo) | 
+                                                        Q(medico__nome__icontains=termo)|
+                                                        Q(data__icontains=termo))
+      else:
+              consultas = MarcarConsulta.objects.order_by('-id', '-paciente')
+      paginator = Paginator(consultas, 10)  
+      page = request.GET.get('page')
+      consultas = paginator.get_page(page)
+      return render(request, 'consulta/consultas.html', {'consultas':consultas})
+@login_required
+def excluir_consulta(request, id):
+        consulta = get_object_or_404(MarcarConsulta, id=id)
+        consulta.delete()
+        messages.add_message(request, constants.SUCCESS, 'Consulta Excluida Sucesso!!')
+        return redirect('listar_consultas')
